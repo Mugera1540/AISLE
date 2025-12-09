2f48f79e700acf65ed7318d8b47d2fa97ff204cf
Mugera <mugera1540@gmail.com>
2025-12-09 14:43:50 +0530

    built backend

---
+++ Backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js"
+
+
+const app =express();
+const PORT=process.env.PORT || 8080;
+
+app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",          // local frontend (for dev)
+  "https://aisle-chi.vercel.app" // your deployed frontend (replace with your actual Vercel URL)
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use("/api",chatRoutes);
+
+app.listen(PORT,()=>{
+  console.log(`Server running on port ${PORT}`);
+connectDB();
+});
+
+const connectDB=async()=>{
+  try{
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to database");
+  }catch(error){
+    console.log("failed to connect to database",error);
+  }
+}
+
+
